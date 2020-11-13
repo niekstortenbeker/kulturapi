f85e962bbbb82d24afdbdf5f29b1f1fdcb44ddc9
Niek Stortenbeker <niekns@gmail.com>
2020-11-13 15:35:56 +0100

    implement database connection to fill response object

--- src/main/java/de/kulturbremen/kulturapi/DataService.java
+++ src/main/java/de/kulturbremen/kulturapi/DataService.java
@@ -1,17 +1,18 @@
 package de.kulturbremen.kulturapi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 
 public class DataService {
-    private static final Logger log = LoggerFactory.getLogger(DataService.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -19,27 +20,64 @@ public class DataService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Show> queryShows() {
-        log.info("query info here maybe:");
-        return jdbcTemplate.query(
-                "SELECT date_time, title, description, url_info, location, category, dubbed FROM shows LIMIT ?",
-                new Object[] { 5 },
+    public Shows queryShows(String start, String stop) {
+        Instant startInstant = convertUserDate(start, Instant.now());
+        Instant stopInstant = convertUserDate(stop, Instant.now().plus(7, ChronoUnit.DAYS));
+        return queryShows(startInstant, stopInstant);
+    }
+
+    public Shows queryShows(Instant start, Instant stop) {
+        Timestamp startTimeStamp= Timestamp.from(start);
+        Timestamp stopTimeStamp = Timestamp.from(stop);
+        List<Show> shows = jdbcTemplate.query(
+                "SELECT date_time, title, description, url_info, location, category, dubbed FROM shows " +
+                "WHERE date_time >= ? and date_time < ? " +
+                "ORDER BY date_time",
+                new Object[] { startTimeStamp, stopTimeStamp },
                 new ShowRowMapper()
         );
+        return new Shows(shows, Instant.now(), start, stop);
+    }
+
+    /**
+     * convert an ISO8601 UTC date string to a ZonedDateTime
+     * @param userDate a ISO8601 UTC date (e.g. 2020-09-30T12:29:29Z)
+     * @param alternativeDate date to use in case userDate is null or parseException is raised
+     * @return a ZonedDateTime representation of that string, or now if fails
+     */
+    private Instant convertUserDate(String userDate, Instant alternativeDate){
+        if (userDate == null) {
+            return alternativeDate;}
+        try {
+            return Instant.parse(userDate);
+        } catch (DateTimeParseException exception) {
+            return alternativeDate;
+        }
+
     }
 
     public static class ShowRowMapper implements RowMapper<Show>{
         @Override
         public Show mapRow(ResultSet rs, int rowNum) throws SQLException {
+
             return new Show(
                     rs.getString("title"),
-                    null,
+                    getDateTime(rs),
                     rs.getString("description"),
                     rs.getString("url_info"),
                     rs.getString("location"),
                     rs.getString("category"),
                     rs.getBoolean("dubbed")
                     );
+        }
+
+        private Instant getDateTime(ResultSet rs) throws SQLException{
+            Timestamp date_time = rs.getTimestamp("date_time");
+            if (date_time != null) {
+                return date_time.toInstant();
+            } else {
+                return null;
+            }
         }
     }
 }
--- src/main/java/de/kulturbremen/kulturapi/ShowsController.java
+++ src/main/java/de/kulturbremen/kulturapi/ShowsController.java
@@ -1,5 +1,7 @@
 package de.kulturbremen.kulturapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,9 @@ import java.util.Map;
 
 @RestController
 public class ShowsController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     /**
      * controller for /shows
@@ -18,6 +23,7 @@ public class ShowsController {
     public Shows show(@RequestParam Map<String,String> allParams){
         // url: /shows/?startdate=2020-09-30T12:29:29Z&stopdate=2020-10-02T12:29:29Z or
         // /shows
-        return Data.getTestShows(allParams.get("startdate"), allParams.get("stopdate"));
+        DataService ds = new DataService(jdbcTemplate);
+        return ds.queryShows(allParams.get("startdate"), allParams.get("stopdate"));
     }
 }
